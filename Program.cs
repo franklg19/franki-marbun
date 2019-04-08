34302be7aee3f69aedf224f920d493abbc63c2dd
Franky Lumbangaol <41195694+franklg19@users.noreply.github.com>
2019-04-08 10:50:45 +0700

    prosedur and function on c#
    program console untuk membuat function dan prosedural

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace latihan_prosedur_dan_fungsi
+{
+    class Program
+    {
+        private static void Garis()
+        {
+            Console.WriteLine(new string('_', 30));
+        }
+        private static void Tulisan()
+        {
+            Console.WriteLine("Belajar fungsi dan prosedur");
+            Console.Write("selesai sampai disini saja");
+        }
+   
+
+        static void Main(string[] args)
+        {
+            
+            Garis();
+            Tulisan();
+            Console.WriteLine();
+            Garis();
+            
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
